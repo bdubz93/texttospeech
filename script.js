f0ad043ab3138d7c02b9089947602bc49b0e53d2
Brandon Winkler <brandonwinkler@live.ca>
2022-04-04 23:49:13 -0400

    base layout and working code uploaded

---
+++ script.js
@@ -0,0 +1,36 @@
+const video = document.querySelector('video');
+const textElement = document.querySelector('[data-text]');
+
+
+async function setup() {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+    video.srcObject = stream
+
+video.addEventListener('playing', async () => {
+    const worker = Tesseract.createWorker()
+    await worker.load()
+    await worker.loadLanguage('eng')
+    await worker.initialize('eng')
+    
+    
+    const canvas = document.createElement('canvas')
+    canvas.width = video.width
+    canvas.height = video.height
+    
+    document.getElementById('btn').addEventListener('click', function () {
+        foo();
+      });
+    async function foo() {
+    canvas.getContext('2d').drawImage(video, 0, 0, video.width, video.height)
+    const {data: { text }} = await worker.recognize(canvas)
+    
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, ' ')))
+
+    textElement.textContent = text
+    }
+})
+};
+
+
+
+setup()
